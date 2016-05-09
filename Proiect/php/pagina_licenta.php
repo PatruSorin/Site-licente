facf9595a493a011838c2daa6b01d43d306b3aeb
Patru Sorin <patrusorin95@gmail.com>
2016-05-10 00:15:15 +0300

    pagina_licenta.php dreaft 1

--- Proiect/php/pagina_licenta.php
+++ Proiect/php/pagina_licenta.php
@@ -1,9 +1,99 @@
 <?php
 
+
+//Obtinem id-ul licentei pentru care trebuie sa generam pagina
 $id=$_SERVER['QUERY_STRING'];
-echo $id ;
-//TO DO: Cod pentru afisearea datelor despre licenta cu id X
 
-//TO DO: Buton de download
+//Interogare baza de date si introducere informatii intr-un vector
+require_once('../mysqli_connect.php');
+$query = "SELECT titlu, descriere, cale_fisier, profesor, firma FROM licente WHERE id='$id'";
+$response = @mysqli_query($dbc, $query);
 
-?>
+$row = mysqli_fetch_array($response);
+
+
+//--------------------------------------------HTML------------------------------------------------------
+
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>LICENTE FMI</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta  charset="UTF-8">
+	<link href="../css/main.css" rel="stylesheet">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+</head>
+
+<body>
+	<div id="header_all" class="row">
+		<div id="header">
+			<div id="logo">
+				<a href="../index.html" title="LicenteFMI"><img src="../img/fmi.png" alt=" " /></a>
+			</div>
+
+			<div id="quickLinks">
+				<div id="header_links">
+					<a href="../login.html"  title="cont companii">cont companii</a> |
+					<a href="../login.html"  title="cont studenti">cont studenti</a> |
+					<a href="../login.html"  title="cont profesori">cont profesori</a>
+				</div>
+			</div>
+
+			<br/>
+
+			<ul id="mainMenu">
+				<li><a href="../index.html"  title="index">Home</a></li>
+				<li><a href="../despre.html"  title="despre">despre</a></li>
+				<li><a href="../listalicente.php"  title="licente">Lista licente</a></li>
+				<li><a href="../contact.html"  title="contact">contact</a></li>
+				<li><a href="../chat.html" title="chat">Chat</a></li>
+			</ul>
+		</div>
+	</div>
+
+	<div id="wrapper_all" class="row">
+		<div id="wrapper" class="col-sm-10">';
+
+    //-------------------------------------CONTINUT PAGINA-------------------------------------------------------------
+    	echo '<div id="content">';
+
+      echo $row['titlu']."<br>";
+      echo $row['descriere']."<br>";
+      echo $row['profesor']."<br>";
+      echo $row['firma']."<br>";
+     //TO DO: Buton de download
+
+     //TO DO Buton pentru profesori pentru a se asocia la o licenta postata de o firma
+
+      echo '</div>
+
+			<div id="right-side" class="col-sm-2">
+				<div id="parteneriHome" class="col-sm-2" >
+					<h2>Parteneri</h2>
+					<table>
+						<tr>
+							<td align="center" valign="center" width="150" height="80">
+								<a href="../" title="Amazon Development Center Romania"><img src="../img/amazon.jpg" border="0" alt="Amazon Development Center Romania" hspace="0" vspace="0" /></a>
+							</td>
+							<td align="center" valign="center" width="150" height="80">
+								<a href="../" title="SC Info World SRL"><img src="../img/infoworld.jpg" border="0" alt="SC Info World SRL" hspace="0" vspace="0" /></a>
+							</td>
+						</tr>
+						<tr>
+							<td align="center" valign="center" width="150" height="80">
+								<a href="../" title="Intel Romania Software Development Center"><img src="../img/intel.jpg" border="0" alt="Intel Romania Software Development Center" hspace="0" vspace="0" /></a>
+							</td>
+							<td align="center" valign="center" width="150" height="80">
+								<a href="../" title="TeamNet International"><img src="../img/teamnet.jpg" border="0" alt="TeamNet International" hspace="0" vspace="0" /></a>
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>';
